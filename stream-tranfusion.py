74ac82f65a0c6677856ce232868fa6ecaf13cfae
Tupple824 <103430959+Tupple824@users.noreply.github.com>
2022-12-03 17:11:32 +0700

    Update stream-tranfusion.py

--- stream-tranfusion.py
+++ stream-tranfusion.py
@@ -19,12 +19,11 @@ Time  = st.text_input('Masukan Berapa Bulan Sejak Donasi Pertama Dengan Range (2
 transfusion_diagnosis = ''
 
 # membuat tombol untuk prediksi
-if st.button(' Prediksi Mendonorkan Darah'):
+if st.button('Prediksi Mendonorkan Darah'):
     transfusion_prediction = transfusion_model.predict([[Recency, Frequency, Monetary, Time]])
-    
     if(transfusion_prediction[0]==0):
         transfusion_diagnosis = 'Bersedia Mendonorkan Darah'
-    else :
+    else:
         transfusion_diagnosis = 'Tidak Bersedia Mendorokan Darah'
     st.success(transfusion_diagnosis)
 
